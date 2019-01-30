cd203fd882b62998d7293def99d722b636b7e33e
velizar1234 <velizar5513@gmail.com>
2019-01-30 20:48:40 +0000

    Initial commit

---
+++ boots/Class Boots.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace boots
+{
+    class Class_Boots
+    {
+        enum BootType {football=1,cowboy,wellington};
+        int bootType;
+        //int football = (int)Type.football;
+        //int cowboy = (int)Type.cowboy;
+        //int wellington = (int)Type.wellington;
+        public int condition = 100;
+
+        public Class_Boots(int type)
+        {
+            if (bootType == 1)
+            {
+                bootType = (int)BootType.football;
+                Console.WriteLine(bootType);
+            }
+            else
+
+                if (bootType == 2)
+            {
+                bootType = (int)BootType.cowboy;
+                Console.WriteLine(bootType);
+            }
+            else
+            {
+                bootType = (int)BootType.wellington;
+                Console.WriteLine(bootType);
+            }
+        }
+
+            public void Walking()
+            {
+            string strwalk = Console.ReadLine();
+            int walk = int.Parse(strwalk);
+            double condition = 100;
+            }
+
+    }
+}
---
+++ boots/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace boots
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Class_Boots firstone = new Class_Boots(rand.Next(1, 4));
+        }
+    }
+}
